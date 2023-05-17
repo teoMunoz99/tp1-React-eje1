ab42e7a203276a5bb0a331874aa7333380ae7909
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-05-17 00:07:41 -0300

    realice el ejercicio 1 del tp1 de react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HelloWorld from "./components/HelloWorld"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <HelloWorld/>
+    </>
+  )
+}
+
+export default App
